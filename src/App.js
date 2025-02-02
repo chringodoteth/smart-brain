078c17f8cc5a8302404080333d1e4f8537b4fcd2
Christian Kett <christian@christians-imac.home>
2025-02-02 18:05:19 +0000

    updating URL

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/clarifai', {
+      fetch('https://smartbrain-api-f2zd.onrender.com/clarifai', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -75,7 +75,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://smartbrain-api-f2zd.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
