2699757b00cff36e6b92049678d6d0078ff1bb2a
Christian Kett <christian@christians-imac.home>
2025-02-02 18:12:44 +0000

    updating URLs

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://smartbrain-api-f2zd.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
